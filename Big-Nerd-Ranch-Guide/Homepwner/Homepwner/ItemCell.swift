ecbf8c98b5bc9f4490aeff727ce13ab44483dd0a
Radio Shaolin <shaolinfm@gmail.com>
2017-08-07 16:59:17 +0300

    Added custom TableViewCell class

---
+++ Big-Nerd-Ranch-Guide/Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Radio Shaolin on 07.08.17.
+//  Copyright © 2017 Radio Shaolin. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
--- Big-Nerd-Ranch-Guide/Homepwner/Homepwner/ItemsViewController.swift
+++ Big-Nerd-Ranch-Guide/Homepwner/Homepwner/ItemsViewController.swift
@@ -52,27 +52,38 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
     }
     
     override func tableView(_ tableView: UITableView,
                               numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+        return itemStore.allItems.count + 1
     }
     
     override func tableView(_ tableView: UITableView,
                               cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Create an instance of UITableViewCell, with default appearance
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        
-        // Set the text on the cell with the description of the item
-        // that is at the nth index of items,«where n = row this cell
-        // will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
-        
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        
-        return cell
+        if indexPath.row < itemStore.allItems.count {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+            
+            // Set the text on the cell with the description of the item
+            // that is at the nth index of items,«where n = row this cell
+            // will appear in on the tableview
+            let item = itemStore.allItems[indexPath.row]
+            
+            // Configure the cell with the Item
+            cell.nameLabel.text = item.name
+            cell.serialNumberLabel.text = item.serialNumber
+            cell.valueLabel.text = "$\(item.valueInDollars)"
+    
+            return cell
+        } else {
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell_NMI")
+            cell.textLabel?.text = "No more Items!"
+            
+            return cell
+        }
     }
 
     override func tableView(_ tableView: UITableView,
@@ -81,18 +92,56 @@ class ItemsViewController: UITableViewController {
         // If the table view is asking to commit a delete command....
         if editingStyle == .delete {
             let item = itemStore.allItems[indexPath.row]
-            // Remove the item from the store
-            itemStore.removeItem(item)
             
-            // Also remove that row from the table view with an animation
-            tableView.deleteRows(at: [indexPath], with: .automatic)
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action) -> Void in
+                // Remove the item from the store
+                self.itemStore.removeItem(item)
+                // Also remove that row from the table view with an animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
         }
     }
 
+    override func tableView(_ tableView: UITableView,
+                              targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath,
+                              toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        if proposedDestinationIndexPath.row >= itemStore.allItems.endIndex {
+            let indexPath = IndexPath(item: itemStore.allItems.count - 1, section: 0)
+            return indexPath
+
+            // return sourceIndexPath
+        }
+        return proposedDestinationIndexPath
+        
+
+    }
+    
     override func tableView(_ tableView: UITableView,
                               moveRowAt sourceIndexPath: IndexPath,
                               to destinationIndexPath: IndexPath) {
         // Update the model
         itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Remove"
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if indexPath.row >= itemStore.allItems.count {
+            return false
+        } else {
+            return true
+        }
     }
 }
